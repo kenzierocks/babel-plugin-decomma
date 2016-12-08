9522674f0f08da940b74d3d371d05ecb2b6f897d
Kenzie Togami <ket1999@gmail.com>
2016-12-07 21:05:10 -0800

    More fixes

--- src/index.js
+++ src/index.js
@@ -5,9 +5,17 @@ export default function ({ types: t }) {
         name: "decomma",
         visitor: {
             SequenceExpression(path) {
+                if (t.isForStatement(path.parent)) {
+                    // ignore comma in for, it's kinda standard
+                    return;
+                }
                 const last = lastElement(path.node.expressions);
                 const parentBlock = path.scope.block;
                 const parentCtx = path.findParent(p => p.parent === parentBlock);
+                if (parentCtx === null) {
+                    throw path.buildCodeFrameError("parentCtx is null, expected to find block "
+                     + JSON.stringify(parentBlock));
+                }
                 // insert most expressions before the current line
                 path.node.expressions.forEach(expr => {
                     if (expr === last) {
